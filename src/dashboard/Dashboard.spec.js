f40071e28f3f67e3d94ce64b0acb008517abde3a
Jackson McComas <jackson.mccomas@gmail.com>
2019-11-11 16:04:34 -0600

    completed MVP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("Dashboard", () => {
+  it("renders correctly", () => {
+    const wrapper = render(<Dashboard />);
+    expect(wrapper.baseElement).toMatchSnapshot();
+  });
+});
